184b08231171cdf83766773783201fecb2cb9e6c
Jon Leithe <jon@jonleithe.no>
2024-10-05 00:09:51 +0200

    Slowly building the OLED test program

--- oled.cpp
+++ oled.cpp
@@ -12,6 +12,23 @@
 
 int main(void)
 {
+	ssd1306		oled;
+
+	uint8_t		lightUp				= 1;
+	uint8_t		displayTwiAddress	= 0x3c;
+	uint8_t		width				= 128;
+	uint8_t		height				= 64;
+
+
+  	//	Enable output to LED port
+	DDRB |= 0b00100000;
+
+	// Blink LED to visualise start of program
+	oled.twi.blinkLED(0b00100000, 3, 1);
+	_delay_us(500000);
+
+	oled.initDisplay(displayTwiAddress, width, height);
+
 	while(1){
 	}
 }
